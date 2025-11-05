84312484adbd52f048436c7888faa2dc133b8318
Bar Angel <bar.ang16@gmail.com>
2025-11-05 18:35:18 +0200

    adding tiles for background and floors

--- tiles.asm
+++ tiles.asm
@@ -1,7 +1,31 @@
 
 SECTION "Tile data", ROM0
-
 Tiles:
+Background:
+        dw `00000000
+        dw `01010101
+        dw `00000000
+        dw `10101010
+        dw `00000000
+        dw `01010101
+        dw `00000000
+        dw `10101010
+EndBackground:
+
+FloorTiles:
+        dw `00000000
+        dw `00000000
+        dw `00000000
+        dw `11111111
+        dw `21212121
+        dw `23232323
+        dw `33333333
+        dw `33333333
+EndFloorTiles:
+
+EndTiles:
+
+Object:
         dw `00111100
         dw `01222210
         dw `13222231
@@ -10,4 +34,4 @@ Tiles:
         dw `12322321
         dw `01222210
         dw `00111100
-TilesEnd:
+EndObject:
