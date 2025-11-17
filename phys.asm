a84ac7a074261c4818bab8a2ec6debe82fd1a87f
Bar Angel <bar.ang16@gmail.com>
2025-11-17 22:53:54 +0200

    show 8 tiles of debug

--- phys.asm
+++ phys.asm
@@ -37,14 +37,18 @@ Init:
         ld bc, EndTiles - Tiles
         call Memcpy
 
-	ld bc, 0x3412
+	ld bc, 0xefcd
 	push bc
-	ld bc, 0xcefa
+	ld bc, 0xab89
 	push bc
-	ld bc, 0xadde
+	ld bc, 0x6745
+	push bc
+	ld bc, 0x2301
 	push bc
 	ld hl, sp + 0
 	call LoadBytesTiles
+	pop af
+	pop af
 	pop af
 	pop af ;clear the stack
 
--- printer.asm
+++ printer.asm
@@ -1,7 +1,7 @@
 SECTION "Printer", ROM0
 
 DEF NUMERAL      EQU 0x8800
-DEF NUMERAL_SIZE EQU 6
+DEF NUMERAL_SIZE EQU 8
 
 ; @param - pointer to the bytes we wish to print in HL
 LoadBytesTiles:
