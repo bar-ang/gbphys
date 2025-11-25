ee71c0f5bce4c42b23c91eb5f117c116b682136d
Bar Angel <bar.ang16@gmail.com>
2025-11-25 11:43:37 +0200

    binary files are annoying. text is better

--- tools/funcplot.py
+++ tools/funcplot.py
@@ -1,19 +1,29 @@
 import sys
 from functools import partial
 
+ROW_LEN = 8
+JUMP_TIME = 90
+JUMP_HEIGHT = 32
+
 def func(x, t, m):
     return 4*m*x*(t-x) / (t*t)
 
 def main(t, m):
     f = partial(func, t=t, m=m)
     rounds = [round(f(x+1)) - round(f(x)) for x in range(t+1)]
-    unsigned = [x & 0xff for x in rounds]
-    bbs = bytes(unsigned[::-1])
-    print(bbs)
-    with open("jump_func.bin", "wb") as f:
-        f.write(bbs)
+    unsigned = [x & 0xff for x in rounds][::-1]
+    hexs = [f"${t:02X}" for t in unsigned]
+    chunks = [hexs[i:i+ROW_LEN] for i in range(0, len(hexs), ROW_LEN)]
+    rows = ["JumpFunc:"]
+    for chunk in chunks:
+        rows.append("db " + ",".join(chunk))
+    rows.append("EndJumpFunc:")
+
+        
+    with open("assets/jump_func.asm", "w") as f:
+        f.write("\n".join(rows))
 
     return 0
 
 if __name__ == "__main__":
-    sys.exit(main(90, 32) or 0)
+    sys.exit(main(JUMP_TIME, JUMP_HEIGHT) or 0)
