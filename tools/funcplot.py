78606688fb985448310788d5e9275d697cb2e545
Bar Angel <bar.ang16@gmail.com>
2025-11-26 17:28:13 +0200

    improving funcplot.py

--- tools/funcplot.py
+++ tools/funcplot.py
@@ -1,42 +1,45 @@
 import sys
 from functools import partial
 
+FILE = "assets/pseudo_math.asm"
+
 ROW_LEN = 8
 JUMP_TIME = 90
 JUMP_HEIGHT = 32
 
-def func(x, t, m):
+def parabola(x, t, m):
     return 4*m*x*(t-x) / (t*t)
 
-def main(t, m):
-    f = partial(func, t=t, m=m)
-    rounds = [round(f(x+1)) - round(f(x)) for x in range(2*t+1)]
-    devs = [rounds[:t//2], rounds[t//2:t], rounds[t:]]
+def burn_func(file, func, name, to, fromm=0, labels={}):
+    rounds = [round(func(x+1)) - round(func(x)) for x in range(fromm, to+1)]
+    rows = [f"Pseudo{name.capitalize()}:"]
 
-    # import pdb; pdb.set_trace()
-    rows = []
-    for i, roundl in enumerate(devs):
-        unsigned = [x & 0xff for x in roundl]
-        hexs = [f"${t:02X}" for t in unsigned]
-        chunks = [hexs[i:i+ROW_LEN] for i in range(0, len(hexs), ROW_LEN)]
+    accum = []
 
-        if i == 0:
-            rows.append("JumpFunc:")
-        elif i == 1:
-            rows.append(".maximum:")
-        elif i == 2:
-            rows.append(".equalibrium:")
-        else:
-            rows.append(f".segments_{i+1}")
-        for chunk in chunks:
-            rows.append("db " + ",".join(chunk))
-    rows.append("EndJumpFunc:")
+    for i, r in enumerate(rounds):
+        if i in labels:
+            rows.append(f".{labels[i].lower()}:")
+        accum.append(f"${(r & 0xff):02X}")
 
-        
-    with open("assets/jump_func.asm", "w") as f:
-        f.write("\n".join(rows))
+        if len(accum) >= ROW_LEN or (i+1) in labels or i == len(rounds)-1:
+            rows.append(f"db {", ".join(accum)}")
+            accum = []
+
+    rows.append(f"End{rows[0]}")
+    file.write("\n".join(rows))
+
+def main():
+    parabola0 = partial(parabola, t=JUMP_TIME, m=JUMP_HEIGHT)
+    
+    with open(FILE, "w") as f:
+        f.write("SECTION \"Pseudo Math\", ROM0\n\n")
+        burn_func(f, parabola0, "Parabola", 2*JUMP_TIME, labels={
+            JUMP_TIME//2: "maximum",
+            JUMP_TIME: "equalibrium"
+        })
+
 
     return 0
 
 if __name__ == "__main__":
-    sys.exit(main(JUMP_TIME, JUMP_HEIGHT) or 0)
+    sys.exit(main() or 0)
