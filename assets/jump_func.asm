80c862536bdf1d3868cc47b3aea5e4e3ecdbcbea
Bar Angel <bar.ang16@gmail.com>
2025-11-26 14:39:26 +0200

    'jump_func' can now handle jumps where the fall is higher than the jump itself

--- assets/jump_func.asm
+++ assets/jump_func.asm
@@ -12,4 +12,17 @@ db $FF,$00,$FF,$00,$FF,$FF,$00,$FF
 db $FF,$FF,$FF,$00,$FF,$FF,$FF,$FF
 db $FF,$FF,$FF,$FF,$FE,$FF,$FF,$FF
 db $FE,$FF,$FF,$FE,$FF
+.equalibrium:
+db $FF,$FE,$FF,$FE,$FE,$FF,$FE,$FF
+db $FE,$FE,$FE,$FF,$FE,$FE,$FE,$FE
+db $FE,$FE,$FE,$FE,$FE,$FE,$FE,$FE
+db $FE,$FD,$FE,$FE,$FD,$FE,$FE,$FD
+db $FE,$FD,$FE,$FD,$FE,$FD,$FD,$FE
+db $FD,$FD,$FE,$FD,$FD,$FD,$FD,$FD
+db $FD,$FD,$FD,$FD,$FD,$FD,$FD,$FD
+db $FD,$FC,$FD,$FD,$FC,$FD,$FD,$FC
+db $FD,$FC,$FD,$FC,$FD,$FC,$FC,$FD
+db $FC,$FC,$FC,$FD,$FC,$FC,$FC,$FC
+db $FC,$FC,$FC,$FC,$FC,$FC,$FC,$FB
+db $FC,$FC,$FC
 EndJumpFunc:
