2f45c95288f22c3c1d287481f798a6e974ac36eb
Bar Angel <bar.ang16@gmail.com>
2025-12-07 15:34:04 +0200

    now we make a map as an .asm file directly from image!

--- tools/make_map.py
+++ tools/make_map.py
@@ -1,28 +1,49 @@
 import sys
-import json
+from PIL import Image
 
-def main(json_file, output_file):
-    with open(json_file, "r") as f:
-        js = json.load(f)
+def image_to_matrix(path, color_map):
+    """
+    Convert an image into a matrix where each unique color becomes an integer ID.
+    Returns (matrix, color_map).
+    """
 
-    with open(output_file, "wb") as f:
-        for row in js:
-            f.write(bytes(row))
+    img = Image.open(path).convert("RGB")
+    width, height = img.size
+    pixels = img.load()
 
-    return 0
+    next_id = 0
+
+    matrix = []
+
+    for y in range(height):
+        row = []
+        for x in range(width):
+            rgb = pixels[x, y]
+
+            if rgb not in color_map:
+                raise Exception(f"unexpected color: {rgb}")
+
+            row.append(color_map[rgb])
+        matrix.append(row)
+
+    return matrix
+
+def make_tilemap(f, matrix, max_width=32):
+    for i, row in enumerate(matrix):
+        full_row = row + [0] * (max_width - len(row))
+        f.write("db " + ", ".join([f"${v:02X}" for v in full_row]) + "\n") 
+
+def main():
+    matrix = image_to_matrix(sys.argv[1], color_map={
+            (255, 255, 255): 0,
+            (0, 0, 0):       1,
+            (15, 0, 255):    2
+        }
+    )
+
+    with open("assets/tilemap.asm", "w") as f:
+        make_tilemap(f, matrix)
+
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("must provide JSON file")
-        sys.exit(-1)
-
-    with open(sys.argv[1], "r") as f:
-        j = json.load(f)
-
-    json_file = sys.argv[1]
-    out_file = "tilemap.2bpp"
-    if len(sys.argv) > 2:
-        out_file = sys.argv[2]
-        
-    sys.exit(main(json_file, out_file) or 0)
-    
+    sys.exit(main() or 0)
